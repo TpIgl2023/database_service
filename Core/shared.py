ce8a96e707a5e0f4564c113ff36b2c2f9a47af52
Fares Mezenner <faresmezenner@gmail.com>
2023-12-14 11:50:21 +0100

    fixed error for empty body

--- Core/shared.py
+++ Core/shared.py
@@ -7,7 +7,10 @@ def check_field_existence(json: dict, field: str):
 
 async def get_request_body(request):
     body_bytes = await request.body()
+
     body_str = body_bytes.decode("utf-8")
+    if not body_str:
+        return {}
     body = json.loads(body_str)
 
     return body
